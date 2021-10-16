c9333ba13a533fd0909293b1cbdd561e5cc8be1b
Dawn LeBlanc <dawnleblanc11@gmail.com>
2021-10-16 05:45:13 -0400

    revert to officehours random method

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+const passwordEl = document.getElementById('password');
+var generateBtn = document.getElementById('generate');
+
 // Upon Generate Password Button (generateBtn) on Click (generateBtn.addEventListener("click",writePassword))
 // Per AboutME: via a series of prompts
 // User will be asked first what length they would like their password to be
@@ -32,7 +33,6 @@ if (userpwLength > 128 || userpwLength < 8) {
 if (userpwUpper + userpwLower + userpwNumeric + userpwSpecial === 0) {
   window.alert("Please select at least one character type");
 }
-
 //Setting up starting arrays 
 let upperArr = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 let lowerArr = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
@@ -41,50 +41,52 @@ let numericArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 let specialArr = ['\u0020','\u0021','\u0022','\u0023','\u0024','\u0025', '\u0026','\u0027', '\u0028', '\u0029', '\u002A','\u002B', '\u002C', '\u002D', '\u002E', '\u002F', '\u003B', '\u003C', '\u003D', '\u003E', '\u003F', '\u005B', '\u005C', '\u005D', '\u005E', '\u0060', '\u007B', '\u007C', '\u007D', '\u007E'];
 //use the unicodes for special characters to avoid syntax problems in the array
 
-let combineduseranswers = [userpwUpper,userpwLower,userpwNumeric,userpwSpecial];
+const randomFunc = {
+  upper: generateRandomUpper,
+  lower: generateRandomLower,
+  numeric: generateRandomNumeric,
+  special: generateRandomSpecial
+}
+	
+function generatePassword(userpwUpper, userpwLower, userpwNumeric, userpwSpecial, userpwLength) {
+	let generatedPassword = '';
+	const typesCount = upper + lower + numeric + special;
+	const typesArr = [{upper}, {lower}, {numeric}, {special}].filter(item => Object.values(item)[0]);
 
-let finalPassword =[];
+  // create a loop
+	for(let i=0; i<length; i+=typesCount) {
+		typesArr.forEach(type => {
+			const typeName = Object.keys(type)[0];
+			generatedPassword += getRandomValues[typeName]();
+		});
+	}
+	
+	const finalPassword = generatedPassword.slice(0, length);
+	
+	return finalPassword;
+}
+
 
 //if userselction == true, shuffle (associatedarray) and place into final password array;
-function generatePasswordupper( ) {
-  if (userpwUpper == 'true'); {
-  shuffle(upperArr);
-  for (let i=0; i<userpwLength;i++) {
-  finalPassword[i] = upperArr[i];
-  }
-  console.log(finalPassword);
-  }
+
+
+function generateRandomUpper( ) {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
 
-function generatePasswordlower( ) {
-  if (userpwLower == 'true'); {
-  shuffle(lowerArr);
-  for (let i=0; i<userpwLength;i++) {
-  finalPassword[i] = lowerArr[i];
-  }
-  console.log(finalPassword);
-  }
+function generateRandomLower( ) {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
 
-function generatePasswordnumeric( ) {
-  if (userpwNumeric == 'true'); {
-  shuffle(numericArr);
-  for (let i=0; i<userpwLength;i++) {
-  finalPassword[i] = numericArr[i];
-  }
-  console.log(finalPassword);
-  }
+function generateRandomNumeric( ) {
+  return +String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
 
-function generatePasswordspecial( ) {
-  if (userpwSpecial == 'true'); {
-  shuffle(specialArr);
-  for (let i=0; i<userpwLength;i++) {
-  finalPassword[i] = specialArr[i];
-  }
-  console.log(finalPassword);
-  }
+function generateRandomSpecial( ) {
+  const special = ['\u0020','\u0021','\u0022','\u0023','\u0024','\u0025', '\u0026','\u0027', '\u0028', '\u0029', '\u002A','\u002B', '\u002C', '\u002D', '\u002E', '\u002F', '\u003B', '\u003C', '\u003D', '\u003E', '\u003F', '\u005B', '\u005C', '\u005D', '\u005E', '\u0060', '\u007B', '\u007C', '\u007D', '\u007E'];
+  return special[Math.floor(Math.random() * special.length)];
 }
+
 
 //used Fisher Yates Shuffle method swap elements array[i] and array[j]
 //https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
@@ -95,22 +97,24 @@ function shuffle(array) {
     }
   }
 
+
 //take from all the characters 
 //randomize the order in which they appear in the password
+generateRandomUpper();
+generateRandomLower();
+generateRandomNumeric();
+generateRandomSpecial();
 
-//var finalPW = generatePassword.slice(0,pwLength)
 
 // Write password to the #password input
-//function writePassword() {
-//  var password = generatePassword();
-//  var passwordText = document.querySelector("#password");
+  function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+  }
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword());
+writePassword();
+var password = document.createElement('span')
+passwordEl.innerHTML = generatePassword(hasUpper, hasLower, hasNumeric, hasSpecial, length);
 
-//  passwordText.value = password;
-
-
-generatePasswordupper();
-generatePasswordlower();
-generatePasswordnumeric();
-generatePasswordspecial();
-
-generateBtn.addEventListener("click", writePassword);
