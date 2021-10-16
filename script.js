7cbc5a9b2288b7c4871a6a48167f473c5bb14b1b
Dawn LeBlanc <dawnleblanc11@gmail.com>
2021-10-16 06:00:22 -0400

    write password function changes

--- script.js
+++ script.js
@@ -33,13 +33,6 @@ if (userpwLength > 128 || userpwLength < 8) {
 if (userpwUpper + userpwLower + userpwNumeric + userpwSpecial === 0) {
   window.alert("Please select at least one character type");
 }
-//Setting up starting arrays 
-let upperArr = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-let lowerArr = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-let numericArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-//source of special charaters unicode https://owasp.org/www-community/password-special-characters
-let specialArr = ['\u0020','\u0021','\u0022','\u0023','\u0024','\u0025', '\u0026','\u0027', '\u0028', '\u0029', '\u002A','\u002B', '\u002C', '\u002D', '\u002E', '\u002F', '\u003B', '\u003C', '\u003D', '\u003E', '\u003F', '\u005B', '\u005C', '\u005D', '\u005E', '\u0060', '\u007B', '\u007C', '\u007D', '\u007E'];
-//use the unicodes for special characters to avoid syntax problems in the array
 
 const randomFunc = {
   upper: generateRandomUpper,
@@ -48,7 +41,7 @@ const randomFunc = {
   special: generateRandomSpecial
 }
 	
-function generatePassword(userpwUpper, userpwLower, userpwNumeric, userpwSpecial, userpwLength) {
+function generatePassword(upper, lower, numeric, special, length) {
 	let generatedPassword = '';
 	const typesCount = upper + lower + numeric + special;
 	const typesArr = [{upper}, {lower}, {numeric}, {special}].filter(item => Object.values(item)[0]);
@@ -66,9 +59,8 @@ function generatePassword(userpwUpper, userpwLower, userpwNumeric, userpwSpecial
 	return finalPassword;
 }
 
-
-//if userselction == true, shuffle (associatedarray) and place into final password array;
-
+// define arrays from table of ASCI information or special characters
+// randomize selction
 
 function generateRandomUpper( ) {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
@@ -86,35 +78,18 @@ function generateRandomSpecial( ) {
   const special = ['\u0020','\u0021','\u0022','\u0023','\u0024','\u0025', '\u0026','\u0027', '\u0028', '\u0029', '\u002A','\u002B', '\u002C', '\u002D', '\u002E', '\u002F', '\u003B', '\u003C', '\u003D', '\u003E', '\u003F', '\u005B', '\u005C', '\u005D', '\u005E', '\u0060', '\u007B', '\u007C', '\u007D', '\u007E'];
   return special[Math.floor(Math.random() * special.length)];
 }
-
-
-//used Fisher Yates Shuffle method swap elements array[i] and array[j]
-//https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
-function shuffle(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-      let j = Math.floor(Math.random() * (i + 1)); // random index from 0 to i
-      [array[i], array[j]] = [array[j], array[i]];
-    }
-  }
-
-
-//take from all the characters 
-//randomize the order in which they appear in the password
-generateRandomUpper();
-generateRandomLower();
-generateRandomNumeric();
-generateRandomSpecial();
+//use the unicodes for special characters to avoid syntax problems in the array
+//source of special charaters unicode https://owasp.org/www-community/password-special-characters
 
 
 // Write password to the #password input
   function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
+  document.createElement('span')
+  passwordEl.innerHTML = generatePassword();
+  var passwordText = document.querySelector("password");
   }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword());
+
 writePassword();
-var password = document.createElement('span')
-passwordEl.innerHTML = generatePassword(hasUpper, hasLower, hasNumeric, hasSpecial, length);
 
