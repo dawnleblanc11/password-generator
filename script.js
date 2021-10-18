57e1ce0963ed867ffc1ff3dc2812a84670e24c33
Dawn LeBlanc <dawnleblanc11@gmail.com>
2021-10-17 21:40:02 -0400

    clean syntax comments

--- script.js
+++ script.js
@@ -1,79 +1,6 @@
 const passwordEl = document.getElementById("password");
 var generateBtn = document.getElementById("generate");
 
-const randomFunc = {
-  upper: generateRandomUpper,
-  lower: generateRandomLower,
-  numeric: generateRandomNumeric,
-  special: generateRandomSpecial,
-};
-
-function generatePassword() {
-  passwordEl.innerHTML = "your secure password is: ";
-  // Per AboutME: via a series of prompts
-  // User will be asked first what length they would like their password to be
-  // Send an error message if outside range
-  // Then verify via click ok on prompt if they want Upper, Lower, Numeric or Special Characters
-
-  // Record the password length the user selects- restricted between 8-128 in userpwLength
-  var userpwLength = window.prompt(
-    "Please choose a password length between 8 and 128 characters:"
-  );
-  if (userpwLength > 128 || userpwLength < 8) {
-    window.alert(
-      "Reminder your password length needs to be between 8 and 128 characters:"
-      );
-  } else {
-    //Identify the user selections and capturing the boolean value in the variable
-    var userpwUpper = window.confirm(
-      "Would you like the password to include Uppercase letters?"
-    );
-    var userpwLower = window.confirm(
-      "Would you like the password to include lowercase letters?"
-    );
-    var userpwNumeric = window.confirm(
-      "Would you like the password to include numbers?"
-    );
-    var userpwSpecial = window.confirm(
-      "For a stronger password, would you like to include special characters?"
-    );
-  }
-  if (userpwUpper + userpwLower + userpwNumeric + userpwSpecial === 0) {
-    window.alert("Please select at least one character type");
-  }
-  if (isNaN(userpwLength)===true)  {
-    window.alert("Please select a <u>NUMBER<u> for your password length");
-  }
-  
-  
-  let upper = userpwUpper;
-  let lower = userpwLower;
-  let numeric = userpwNumeric;
-  let special = userpwSpecial;
-  let length = + userpwLength;
-  
-  let generatedPassword = " ";
-  
-
-  const typesCount = upper + lower + numeric + special;
-
-  const typesArr = [{ upper }, { lower }, { numeric }, { special }].filter(
-    (item) => Object.values(item)[0]
-  );
-
-  // create a loop
-  for (let i = 0; i < length; i += typesCount) {
-    typesArr.forEach((type) => {
-      const funcName = Object.keys(type)[0];
-      generatedPassword += randomFunc[funcName]();
-    });
-  }
-
-  const finalPassword = generatedPassword.slice(0,(length+1));
-  // Write password to the #password input
-  passwordEl.innerHTML += finalPassword;
-}
-
 // define arrays from table of ASCI information or special characters
 // randomize selection
 
@@ -96,6 +23,85 @@ function generateRandomSpecial() {
 }
 //use the unicodes for special characters to avoid syntax problems in the array
 //source of special charaters unicode https://owasp.org/www-community/password-special-characters
+
+//intitalize values globally from user input
+const randomFunc = {
+  upper: generateRandomUpper,
+  lower: generateRandomLower,
+  numeric: generateRandomNumeric,
+  special: generateRandomSpecial,
+};
+//main function
+function generatePassword() {
+  passwordEl.innerHTML = ("your secure password is: ");
+  // Per AboutME: via a series of prompts
+  // User will be asked first what length they would like their password to be
+  // Send an error message if outside range
+  // Then verify via click ok on prompt if they want Upper, Lower, Numeric or Special Characters
+
+  // Record the password length the user selects- restricted between 8-128 in userpwLength
+  var userpwLength = window.prompt(
+    "Please choose a password length between 8 and 128 characters:"
+  );
+//validate length
+  if (userpwLength > 128 || userpwLength < 8) {
+    window.alert(
+      "Reminder your password length needs to be between 8 and 128 characters:"
+      );
+  } 
+  // continue with user input
+  else {
+    //Identify the user selections and capturing the boolean value in the variable
+    var userpwUpper = window.confirm(
+      "Would you like the password to include Uppercase letters?"
+    );
+    var userpwLower = window.confirm(
+      "Would you like the password to include lowercase letters?"
+    );
+    var userpwNumeric = window.confirm(
+      "Would you like the password to include numbers?"
+    );
+    var userpwSpecial = window.confirm(
+      "For a stronger password, would you like to include special characters?"
+    );
+  };
+// validate selections
+  if (userpwUpper + userpwLower + userpwNumeric + userpwSpecial === 0) {
+    window.alert("Please select at least one character type");
+  };
+//validate integer for Password Length
+  if (isNaN(userpwLength)===true)  {
+    window.alert("Please select a NUMBER for your password length");
+  };
+  
+  // set function variables
+  let upper = userpwUpper;
+  let lower = userpwLower;
+  let numeric = userpwNumeric;
+  let special = userpwSpecial;
+  let length = + userpwLength;
+  
+  //intialize variables
+  let generatedPassword = " ";
+  
+
+  const typesCount = upper + lower + numeric + special;
+
+  const typesArr = [{ upper }, { lower }, { numeric }, { special }].filter(
+    (item) => Object.values(item)[0]);
+
+  // create a loop
+  for (let i = 0; i < length; i += typesCount) {
+    typesArr.forEach(type => {
+      const funcName = Object.keys(type)[0];
+      generatedPassword += randomFunc[funcName]();
+    });
+  }
+// cut off array to proper size
+  const finalPassword = generatedPassword.slice(0,(length+1));
+  // Write password to the #password input
+  passwordEl.innerHTML += finalPassword;
+}
 
 // Add event listener to generate button
 // Upon Generate Password Button (generateBtn) on Click (generateBtn.addEventListener("click",writePassword))
